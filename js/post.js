9d74a5006785cd65772aab54757adbed044662e8
Hyun Seo <hyunkseo@icloud.com>
2019-12-11 19:57:06 -0600

    fixed error

--- js/post.js
+++ js/post.js
@@ -1,16 +1,14 @@
+const postForm = document.getElementsByTagName('form')[0];
+const postInput = postForm.getElementsByTagName('input');
+const postTextArea = postForm.getElementsByTagName('textarea')[0];
+const postSubmit = document.getElementsByClassName('post-submit')[0];
 if(window.location.search.length < 4) {
-  const postForm = document.getElementsByTagName('form')[0];
-  const postInput = postForm.getElementsByTagName('input');
-  const postTextArea = postForm.getElementsByTagName('textarea')[0];
   postInput[0].value = '';
   postInput[1].value = '';
   postTextArea.value = '';
 }
 
 const checkInputs = function(){
-  const postForm = document.getElementsByTagName('form')[0];
-  const postInput = postForm.getElementsByTagName('input');
-  const postTextArea = postForm.getElementsByTagName('textarea')[0];
   if(postInput[0].value.length > 0 && postInput[1].value.length > 0 && postTextArea.value.length > 0){
     enableButtons();
   } else {
@@ -18,11 +16,9 @@ const checkInputs = function(){
   }
 }
 const enableButtons = function(){
-  const postSubmit = document.getElementsByClassName('post-submit')[0];
   postSubmit.className = 'button-1 post-submit';
 }
 const disableButtons = function(){
-  const postSubmit = document.getElementsByClassName('post-submit')[0];
   postSubmit.className = 'button-1 post-submit disabled';
 }
 
